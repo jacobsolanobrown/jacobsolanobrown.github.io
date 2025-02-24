ffd4f016d9946fcc6c35a41e6e64c27ba318f4e7
Jacob Solano <113313939+jacobsolanobrown@users.noreply.github.com>
2025-02-24 00:32:29 -0500

    added styling to accessible page

--- jacob-portfolio/src/pages/AccessibleComponentsProject.tsx
+++ jacob-portfolio/src/pages/AccessibleComponentsProject.tsx
@@ -1,22 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/Homepage.css";
+import "../styles/AccessibleComponentsPage.css";
+
 
 const AccessibleComponentsProject: React.FC = () => {
   return (
     <div>
-      <header>
-        <h1><Link to="/">Jacob Solano</Link></h1>
-      </header>
       <main>
         <section>
           <h1>Accessible Components Assignment - CS1300 </h1>
-
           <h2>Chosen Component: Accordion</h2>
           <p>hi.. </p>
         </section>
       </main>
       <footer>
-        <p>&copy; 2025 Jacob Solano</p>
+        <p>
+          <Link to="/">&copy; 2025 jacob solano</Link>
+        </p>
       </footer>
     </div>
   );
